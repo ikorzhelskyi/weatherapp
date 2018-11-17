0211a1d408fce610a9e0b7ad93b1d55044ecdc86
Ihor Korzhelskyi <ihork2109@gmail.com>
2018-11-17 08:32:39 +0200

    Get temperature add condition from Accuweather

--- weatherapp.py
+++ weatherapp.py
@@ -6,13 +6,13 @@
 import html
 from urllib.request import urlopen, Request
 
-ACCU_URL = "https://www.accuweather.com/en/ua/lviv/324561/weather-forecast/324561?lang=en-us"
+ACCU_URL = "https://www.accuweather.com/uk/ua/lviv/324561/weather-forecast/324561"
 
 # getting page from server
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
 accu_request = Request(ACCU_URL, headers=headers)
 accu_page = urlopen(accu_request).read()
-accu_page = str(accu_page)
+accu_page = accu_page.decode('utf-8')
 
 ACCU_TEMP_TAG = '<span class="large-temp">'
 accu_temp_tag_size = len(ACCU_TEMP_TAG)
@@ -25,9 +25,6 @@ for char in accu_page[accu_temp_value_start:]:
 	else:
 	    break
 
-print('AccuWeather: \n')
-print(f'Temperature: {html.unescape(accu_temp)}\n')
-
 ACCU_COND_TAG = '<span class="cond">'
 accu_cond_tag_size = len(ACCU_COND_TAG)
 accu_cond_tag_index = accu_page.find(ACCU_COND_TAG)
@@ -39,4 +36,6 @@ for char in accu_page[accu_cond_value_start:]:
 	else:
 	    break
 
-print(f'Current state: {accu_cond}')
+print('AccuWeather: \n')
+print(f'Temperature: {html.unescape(accu_temp)}\n')
+print(f'Condition: {accu_cond}')
