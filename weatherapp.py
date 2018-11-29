291571e612095bd16075fafb0665e915c3615c9d
Ihor Korzhelskyi <ihork2109@gmail.com>
2018-11-29 23:37:56 +0200

    Get data from the AccuWeather site using the BeautifulSoup library

--- weatherapp.py
+++ weatherapp.py
@@ -6,6 +6,7 @@
 import sys
 import html
 import argparse
+from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 
 ACCU_URL = "https://www.accuweather.com/uk/ua/lviv/324561/weather-forecast/324561"
@@ -48,30 +49,59 @@ def get_tag_content(page_content, tag):
             break
     return content
 
-def get_weather_info(page_content, tags):
-    """Returns information collected from tags.
+def get_weather_info(page_content):
+    """Gets data from the site using the BeautifulSoup library
     """
 
-    return tuple([get_tag_content(page_content, tag) for tag in tags])
+    city_page = BeautifulSoup(page_content, 'html.parser')
+    current_day_section = city_page.find(
+        'li', class_='night current first cl')
 
-def produce_output(provider_name, temp, condition):
+    weather_info = {}
+    if current_day_section:
+        current_day_url = current_day_section.find('a').attrs['href']
+        if current_day_url:
+            current_day_page = get_page_source(current_day_url)
+            if current_day_page:
+                current_day = \
+                    BeautifulSoup(current_day_page, 'html.parser')
+                weather_details = \
+                    current_day.find('div', attrs={'id': 'detail-now'})
+                condition = weather_details.find('span', class_='cond')
+                if condition:
+                    weather_info['cond'] = condition.text
+                temp = weather_details.find('span', class_='large-temp')
+                if temp:
+                    weather_info['temp'] = temp.text
+                feal_temp = weather_details.find('span', class_='small-temp')
+                if feal_temp:
+                    weather_info['feal-temp'] = feal_temp.text
+
+                wind_info = weather_details.find_all('li', class_='wind')
+                if wind_info:
+                    weather_info['wind'] = \
+                        ' '.join(map(lambda t: t.text.strip(),wind_info))
+
+    return weather_info
+
+def produce_output(info):
     """Formats and displays the found data.
     """
-    
-    print(f'\n{provider_name}:\n')
-    print(f'Temperature: {html.unescape(temp)}\n')
-    print(f'Condition: {condition}\n')
+    print('AccuWeather: \n')
+
+    for key, value in info.items():
+        print(f'{key}: {html.unescape(value)}')
 
 def main(argv):
     """Main entry point.
     """
-    
+
     KNOWN_COMMANDS = {'accu': 'AccuWeather', 'rp5': 'RP5', 'sin': 'SINOPTIK'}
 
     parser = argparse.ArgumentParser()
     parser.add_argument('command', help='Service name', nargs=1)
     params = parser.parse_args(argv)
-    
+
     weather_sites = {"AccuWeather": (ACCU_URL, ACCU_TAGS),
                      "RP5": (RP5_URL, RP5_TAGS),
                      "SINOPTIK": (SIN_URL, SIN_TAGS)}
@@ -89,8 +119,7 @@ def main(argv):
     for name in weather_sites:
         url, tags = weather_sites[name]
         content = get_page_source(url)
-        temp, condition = get_weather_info(content, tags)
-        produce_output(name, temp, condition)
+        produce_output(get_weather_info(content))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
