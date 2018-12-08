22b18ca43c855e7f22c2680b65e188ceea7763d8
Ihor Korzhelskyi <ihork2109@gmail.com>
2018-12-08 22:13:27 +0200

    Add function saves page sourse data to file

--- weatherapp.py
+++ weatherapp.py
@@ -5,6 +5,7 @@
 
 import sys
 import html
+import hashlib
 import re
 import argparse
 import configparser
@@ -25,6 +26,8 @@ DEFAULT_URL = ('https://www.accuweather.com/uk/ua/'
 CONFIG_LOCATION = 'Location'
 CONFIG_FILE = 'weatherapp.ini'
 
+CACHE_DIR = '.wappcache'
+
 RP5_URL = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D1%83_'
             '%D0%9B%D1%8C%D0%B2%D0%BE%D0%B2%D1%96,_%D0%9B%D1%8C%D0%B2%D1'
             '%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0_%D0%BE%D0%B1%D0%BB%D0%B0%D1'
@@ -44,6 +47,24 @@ def get_request_headers():
     """
     
     return {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
+
+def get_cache_directory():
+    """Path to cache directory.
+    """
+
+    return Path.home() / CACHE_DIR
+
+def save_cache(url, page_source):
+    """Save page source data to file.
+    """
+
+    url_hash = hashlib.md5(url.encode('utf-8')).hexdigest()
+    cache_dir = get_cache_directory()
+    if not cache_dir.exists():
+        cache_dir.mkdir(parents=True)
+
+    with  (cache_dir / url_hash).open('wb') as cache_file:
+        cache_file.write(page_source)
 
 def get_page_source(url):
     """Gets page source by given url address.
