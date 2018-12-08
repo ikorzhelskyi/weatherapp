ef5662ca3a972f404d7685a4fda3f127b1b2a28a
Ihor Korzhelskyi <ihork2109@gmail.com>
2018-12-09 00:08:57 +0200

    Modify function that gets data from server so that if file with relevant data exists then data from this file was taken.

--- weatherapp.py
+++ weatherapp.py
@@ -48,17 +48,26 @@ def get_request_headers():
     
     return {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
 
+
 def get_cache_directory():
     """Path to cache directory.
     """
 
     return Path.home() / CACHE_DIR
 
+
+def get_url_hash(url):
+    """Generates hash for given url.
+    """
+    
+    return hashlib.md5(url.encode('utf-8')).hexdigest()
+
+
 def save_cache(url, page_source):
     """Save page source data to file.
     """
 
-    url_hash = hashlib.md5(url.encode('utf-8')).hexdigest()
+    url_hash = get_url_hash(url)
     cache_dir = get_cache_directory()
     if not cache_dir.exists():
         cache_dir.mkdir(parents=True)
@@ -66,14 +75,37 @@ def save_cache(url, page_source):
     with  (cache_dir / url_hash).open('wb') as cache_file:
         cache_file.write(page_source)
 
+
+def get_cache(url):
+    """Return cache data if any.
+    """
+
+    cache = b''
+    url_hash = get_url_hash(url)
+    cache_dir = get_cache_directory()
+    if cache_dir.exists():
+        cache_path = cache_dir / url_hash
+        if cache_path.exists():    
+            with cache_path.open('rb') as cache_file:   
+                cache = cache_file.read()
+
+    return cache
+
+
 def get_page_source(url):
     """Gets page source by given url address.
     """
 
-    request = Request(url, headers=get_request_headers())
-    page_source = urlopen(request).read()
-    save_cache(url, page_source)
+    cache = get_cache(url)
+    if cache:
+        page_source = cache
+    else:
+        request = Request(url, headers=get_request_headers())
+        page_source = urlopen(request).read()
+        save_cache(url, page_source)
+
     return page_source.decode('utf-8')
+
 
 def get_locations(locations_url):
     locations_page = get_page_source(locations_url)
@@ -84,7 +116,9 @@ def get_locations(locations_url):
         url = location.find('a').attrs['href']
         location = location.find('em').text
         locations.append((location, url))
+
     return locations
+
 
 def get_configuration_file():
     """Returns path to configuration file in home directory.
@@ -92,14 +126,17 @@ def get_configuration_file():
 
     return Path.home() / CONFIG_FILE
 
+
 def save_configuration(name, url):
     """Save selected location to configuration file.
     """
 
     parser = configparser.ConfigParser()
     parser[CONFIG_LOCATION] = {'name': name, 'url': url}
+
     with open(get_configuration_file(), 'w') as configfile:
        parser.write(configfile)
+
 
 def get_configuration():
     """Returns configured location name and url.
@@ -116,6 +153,7 @@ def get_configuration():
     
     return name, url
 
+
 def configurate():
     locations = get_locations(ACCU_BROWSE_LOCATIONS)
     while locations:
@@ -126,6 +164,7 @@ def configurate():
         locations = get_locations(location[1])
 
     save_configuration(*location)
+
 
 def get_tag_content(page_content, tag):
     """Finds the necessary data in the content of the page.
@@ -141,7 +180,9 @@ def get_tag_content(page_content, tag):
             content += c
         else:
             break
+
     return content
+
 
 def get_weather_info(page_content):
     """Gets data from the site using the BeautifulSoup library
@@ -178,6 +219,7 @@ def get_weather_info(page_content):
 
     return weather_info
 
+
 def produce_output(city_name, info):
     """Formats and displays the found data.
     """
@@ -187,10 +229,12 @@ def produce_output(city_name, info):
     for key, value in info.items():
         print(f'{key}: {html.unescape(value)}')
 
+
 def get_accu_weather_info():
     city_name, city_url = get_configuration()
     content = get_page_source(city_url)
     produce_output(city_name, get_weather_info(content))
+
 
 def main(argv):
     """Main entry point.
