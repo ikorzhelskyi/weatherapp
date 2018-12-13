c708cab817d14dbae0fa9222ff83e44f9983c988
Ihor Korzhelskyi <ihork2109@gmail.com>
2018-12-13 21:46:17 +0200

    Create config.py module and move all constants from weatherapp.py into config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+ACCU_URL = ('https://www.accuweather.com/'
+            'uk/ua/lviv/324561/weather-forecast/324561')
+ACCU_TAGS = ('<span class="large-temp">', '<span class="cond">')
+ACCU_BROWSE_LOCATIONS = 'https://www.accuweather.com/uk/browse-locations'
+
+FAKE_MOZILLA_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'
+
+CONFIG_FILE = 'weatherapp.ini'
+CONFIG_LOCATION = 'Location'
+
+DEFAULT_NAME = 'Lviv'
+DEFAULT_URL = \
+    'https://www.accuweather.com/uk/ua/lviv/324561/weather-forecast/324561'
+
+CACHE_DIR = '.wappcache'
+CACHE_TIME = 300
+
+RP5_URL = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D1%83_'
+            '%D0%9B%D1%8C%D0%B2%D0%BE%D0%B2%D1%96,_%D0%9B%D1%8C%D0%B2%D1'
+            '%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0_%D0%BE%D0%B1%D0%BB%D0%B0%D1'
+            '%81%D1%82%D1%8C')
+RP5_TAGS = ('<span class="t_0" style="display: block;">',
+            '<div class="cn5" onmouseover="tooltip(this, \'<b>')
+
+SIN_URL = ('https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE'
+           '%D0%B4%D0%B0-%D0%BB%D1%8C%D0%B2%D1%96%D0%B2')
+SIN_TAGS = ('<p class="today-temp">'
+            '<div class="description"> <!--noindex-->')
