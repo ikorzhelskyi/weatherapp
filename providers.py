0e7bf9cdbaa646c40f803f9c245086d8af1d9871
Ihor Korzhelskyi <ihork2109@gmail.com>
2018-12-22 06:12:10 +0200

    Implementation  RP5  provider caching  in class Rp5WeatherProvider methods

--- providers.py
+++ providers.py
@@ -238,3 +238,51 @@ class Rp5WeatherProvider:
 
         with open(self.get_configuration_file(), 'w') as configfile:
             parser.write(configfile)
+
+    def get_request_headers(self):
+        """Returns custom headers for url request.
+        """
+
+        return {'User-Agent': config.FAKE_MOZILLA_AGENT}
+
+    def get_url_hash(self, url):
+        """Generates url hash.
+        """
+
+        return hashlib.md5(url.encode('utf-8')).hexdigest()
+
+    def get_cache_directory(self):
+        """Path to cache directory.
+        """
+
+        return Path.home() / config.CACHE_DIR
+
+    def is_valid(self, path):
+        """Checks if current cache is valid.
+        """
+
+        return (time.time() - path.stat().st_mtime) < config.CACHE_TIME
+
+    def get_cache(self, url):
+        """Returns cache by given url address if any.
+        """
+
+        cache = b''
+        cache_dir = self.get_cache_directory()
+        if cache_dir.exists():
+            cache_path = cache_dir / self.get_url_hash(url)
+            if cache_path.exists() and self.is_valid(cache_path):
+                with cache_path.open('rb') as cache_file:
+                    cache = cache_file.read()
+        return cache
+
+    def save_cache(self, url, page_source):
+        """Save page source data to file.
+        """
+
+        cache_dir = self.get_cache_directory()
+        if not cache_dir.exists():
+            cache_dir.mkdir(parents=True)
+
+        with  (cache_dir / self.get_url_hash(url)).open('wb') as cache_file:
+            cache_file.write(page_source)
