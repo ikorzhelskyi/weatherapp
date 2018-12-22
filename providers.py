fc1dc37bf81a44acbe1f237c873b2f2ccd3f96bb
Ihor Korzhelskyi <ihork2109@gmail.com>
2018-12-22 06:05:21 +0200

    Implementation  RP5  provider configuration  in class Rp5WeatherProvider methods

--- providers.py
+++ providers.py
@@ -200,3 +200,41 @@ class Rp5WeatherProvider:
         location, url = self.get_configuration()
         self.location = location
         self.url = url
+
+    def get_configuration_file(self):
+        """Returns path to configuration file in home directory.
+        """
+
+        return Path.home() / config.CONFIG_FILE
+
+    def get_configuration(self):
+        """Returns configured location name and url.
+
+        :return: city name and url
+        :rtype: tuple
+        """
+
+        name = config.DEFAULT_LOCATION_NAME
+        url = config.RP5_DEFAULT_LOCATION_URL
+        configuration = configparser.ConfigParser()
+        configuration.read(self.get_configuration_file())
+        if config.CONFIG_LOCATION in configuration.sections():
+            location_config = configuration[config.CONFIG_LOCATION]
+            name, url = location_config['name'], location_config['url']
+        return name, url
+
+    def save_configuration(self, name, url):
+        """Save selected location to configuration file.
+
+        :param name: city name
+        :param type: str
+
+        :param url: preferred location URL
+        :param type: str
+        """
+
+        parser = configparser.ConfigParser()
+        parser[config.CONFIG_LOCATION] = {'name': name, 'url': url}
+
+        with open(self.get_configuration_file(), 'w') as configfile:
+            parser.write(configfile)
