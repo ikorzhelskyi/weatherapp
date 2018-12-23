7cc0f4acc6e4f697aa11b11ec03ecd17005f41e4
Ihor Korzhelskyi <ihork2109@gmail.com>
2018-12-23 07:38:01 +0200

    Delete unused constants in config.py module

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 ACCU_PROVIDER_NAME = 'accu'
 ACCU_URL = ('https://www.accuweather.com/'
             'uk/ua/lviv/324561/weather-forecast/324561')
-ACCU_TAGS = ('<span class="large-temp">', '<span class="cond">')
 ACCU_BROWSE_LOCATIONS = 'https://www.accuweather.com/uk/browse-locations'
 
 FAKE_MOZILLA_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'
@@ -20,16 +19,8 @@ RP5_PROVIDER_NAME = 'rp5'
 RP5_URL = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D1%83_%D0%9B'
            '%D1%8C%D0%B2%D0%BE%D0%B2%D1%96,_%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1'
            '%81%D1%8C%D0%BA%D0%B0_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C')
-
-RP5_TAGS = ('<span class="t_0" style="display: block;">',
-            '<div class="cn5" onmouseover="tooltip(this, \'<b>')
 RP5_BROWSE_LOCATIONS = 'http://rp5.ua/Weather_in_the_world'
 RP5_DEFAULT_LOCATION_URL = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0'
             '%B0_%D1%83_%D0%9B%D1%8C%D0%B2%D0%BE%D0%B2%D1%96,_%D0%9B%D1%8C%D0'
             '%B2%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0_%D0%BE%D0%B1%D0%BB%D0%B0'
             '%D1%81%D1%82%D1%8C')
-
-SIN_URL = ('https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE'
-           '%D0%B4%D0%B0-%D0%BB%D1%8C%D0%B2%D1%96%D0%B2')
-SIN_TAGS = ('<p class="today-temp">'
-            '<div class="description"> <!--noindex-->')
