2e4d7a746928b8edaaed6a547f048968dc393bc3
Ihor Korzhelskyi <ihork2109@gmail.com>
2018-12-25 23:44:20 +0200

    Implement a decorator that slows down a function for an arbitrary number of seconds

--- decorators.py
+++ decorators.py
@@ -3,6 +3,7 @@
 
 import time
 
+
 def one_moment(func):
     """Waits one second before calling function.
     """
@@ -11,3 +12,16 @@ def one_moment(func):
         time.sleep(1)
         return func(*args, **kwargs)
     return wrapper
+
+
+def slow_down(sec=1):
+
+    def one_moment(func):
+        """Waits one second before calling function.
+        """
+
+        def wrapper(*args, **kwargs):
+            time.sleep(sec)
+            return func(*args, **kwargs)
+        return wrapper
+    return one_moment
