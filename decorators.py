a4e107288b6ef695941179dfef6043d2b9c3aece
Ihor Korzhelskyi <ihork2109@gmail.com>
2018-12-26 00:14:51 +0200

    Implement a decorator that measures execution time of a function

--- decorators.py
+++ decorators.py
@@ -25,3 +25,16 @@ def slow_down(sec=1):
             return func(*args, **kwargs)
         return wrapper
     return one_moment
+
+
+def timer(func):
+    """Prints the runtime of the decorated function.
+    """
+
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        run_time = time.perf_counter() - start_time
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return result
+    return wrapper
