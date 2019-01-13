3312d3f4efb455e2c17d5cc0a8673e621a07f120
Ihor Korzhelskyi <ihork2109@gmail.com>
2019-01-14 00:10:54 +0200

    Implement the ProviderManager class that represents the container for working with providers and implements the interface of the Manager abstract class

--- providermanager.py
+++ providermanager.py
@@ -1,39 +1,16 @@
 from providers import AccuWeatherProvider, Rp5WeatherProvider
 
+import commandmanager
 
-class ProviderManager:
+
+class ProviderManager(commandmanager.CommandManager):
 
     """ Discovers registered providers and loads them.
     """
 
-    def __init__(self):
-        self._providers = {}
-        self._load_providers()
-
-    def _load_providers(self):
+    def _load_commands(self):
         """ Loads all existing providers.
         """
 
         for provider in [AccuWeatherProvider, Rp5WeatherProvider]:
             self.add(provider.name, provider)
-    
-    def add(self, name, provider):
-        """ Adds new provider by name.
-        """
-
-        self._providers[name] = provider
-
-    def get(self, name):
-        """ Gets provider by name.
-        """
-
-        return self._providers.get(name, None)
-
-    def __len__ (self):
-        return len(self._providers)
-
-    def __contains__(self, name):
-        return name in self._providers
-
-    def __getitem__(self, name):
-        return self._providers[name]
